32e29054b249f3484affa1dc09c3d28a37361491
Bruno Paula <devbrunopaula@gmail.com>
2021-06-11 12:19:09 -0400

    projects table completed

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,20 @@
-// build your `Project` model here
+/*
+|--------------------------------------------------------------------------
+|  Project Model
+|--------------------------------------------------------------------------
+
+*/
+const find = () => {}
+
+const findById = (project_id) => {}
+
+const add = (project) => {}
+
+const addStep = (project_id, step) => {}
+
+module.exports = {
+	find,
+	findById,
+	add,
+	addStep,
+}
