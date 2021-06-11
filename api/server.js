a4f7f669e5d04482fc57a18b20cf4f641ccdc5c0
Bruno Paula <devbrunopaula@gmail.com>
2021-06-11 11:15:30 -0400

    WIP, setup and project get route

--- api/server.js
+++ api/server.js
@@ -1,7 +1,13 @@
 'use strict'
 const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const ProjectController = require('./project/router')
 
 const server = express()
-
+server.use(cors())
 server.use(express.json())
-server.use()
+server.use(morgan('tiny'))
+server.use('/api/projects', ProjectController)
+
+module.exports = server
